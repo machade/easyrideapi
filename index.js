ed239cb51810c0a0df959f9055918ce8b559a818
machade <matheus-machado1@hotmail.com>
2017-09-21 16:22:24 -0300

    Alterações

---
+++ index.js
@@ -0,0 +1,49 @@
+const express = require('express');
+const app = express();         
+const bodyParser = require('body-parser');
+const port = 3000; //porta padrão
+const mysql = require('mysql');
+
+//configurando o body parser para pegar POSTS mais tarde
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+const connection = mysql.createConnection({
+  host     : 'localhost',
+  port     : 3306,
+  user     : 'root',
+  password : 'mathe147',
+  database : 'EasyRide'
+});
+
+
+//definindo as rotas
+const router = express.Router();
+router.get('/', (req, res) => res.json({ message: 'Funcionando!' }));
+app.use('/', router);
+
+//inicia o servidor
+app.listen(port);
+console.log('API funcionando!');
+
+function execSQLQuery(sqlQry, res){
+  const connection = mysql.createConnection({
+    host     : 'localhost',
+    port     : 3306,
+    user     : 'root',
+    password : 'mathe147',
+    database : 'EasyRide'
+  });
+
+  router.get('/clientes', (req, res) =>{
+    execSQLQuery('SELECT * FROM Tipo_Usuario', res);
+  })
+  
+  connection.query(sqlQry, function(error, results, fields){
+      if(error) 
+        res.json(error);
+      else
+        res.json(results);
+      connection.end();
+      console.log('executou!');
+  });
+}
