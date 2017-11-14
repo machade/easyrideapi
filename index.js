340647662b9b59a07134eeb4600cddb20da0b8d5
machade <matheus-machado1@hotmail.com>
2017-11-14 20:36:47 -0200

    atualização de rota

--- index.js
+++ index.js
@@ -10,11 +10,11 @@ app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 const connection = mysql.createConnection({
-  host     : 'localhost',
+  host     : 'sql10.freesqldatabase.com',
   port     : 3306,
-  user     : 'root',
-  password : 'mathe147',
-  database : 'EasyRide'
+  user     : 'sql10204309',
+  password : 'YL2Hu2w9F5',
+  database : 'sql10204309'
 });
 
 
@@ -32,11 +32,11 @@ console.log('API funcionando!');
 
 function execSQLQuery(sqlQry, res){
   const connection = mysql.createConnection({
-    host     : 'localhost',
+    host     : 'sql10.freesqldatabase.com',
     port     : 3306,
-    user     : 'root',
-    password : 'mathe147',
-    database : 'EasyRide'
+    user     : 'sql10204309',
+    password : 'YL2Hu2w9F5',
+    database : 'sql10204309'
   });
   connection.query(sqlQry, function(error, results, fields){
       if(error) 
@@ -63,7 +63,7 @@ router.get('/disponibilidade/:id_rota', (req, res) =>{
 })
 
 router.get('/dest_usuario', (req, res) =>{
-  execSQLQuery('SELECT * FROM local WHERE ID_USUARIO= 2', res);
+  execSQLQuery('SELECT * FROM local WHERE ID_USUARIO = 2', res);
 })
 
 router.get('/tipo_rota', (req, res) =>{
@@ -100,7 +100,7 @@ router.get('/listarRota', (req, res) =>{
             'INNER JOIN local  as lo ON lo.id = rota.id_origem '+
             'INNER JOIN local  as ld ON ld.id = rota.id_destino '+
             'INNER JOIN tipo_rota as tr on tr.id = rota.id_TipoRota '+
-            'where rota.id_usuario = 3';
+            'where rota.id_usuario = 3 and dataCriacao = "12-11-2017"';
 
   execSQLQuery(sql, res);
 })
@@ -138,6 +138,24 @@ router.get('/ListaCaronasVolta/:destino&&:hora1&&:hora2&&:dateString',( req,res)
   execSQLQuery(sql,res);
 })
 
+router.get('/RotaCarona/:id_usuario&&:id_TipoRota&&:dateString',( req,res) => {
+  var sql = 'select rota.id as id_rota, id_origem, origem.localizacao as origem,id_destino,destino.localizacao as destino from rota '+ 
+            'inner join local as origem On origem.id = rota.id_origem ' +
+            'inner join local as destino on destino.id = rota.id_destino ' +
+            'where rota.id_usuario =' + req.params.id_usuario +
+            ' and rota.id_TipoRota = ' + req.params.id_TipoRota +
+            ' and rota.dataCriacao = "' + req.params.dateString +'"';
+  execSQLQuery(sql,res);
+})
+
+router.get('/CaronaLocalizacoes/:id_local',( req,res) => {
+  var sql = 'select id_local, lo.localizacao as localizacao from carona '+
+            'inner join local as lo on lo.id = carona.id_local '+
+            'where id_rota = '+ req.params.id_local+
+            ' and status = 1 ';
+  execSQLQuery(sql,res);
+})
+
 //POST------------------------------------------------------
 router.post('/rota/novo', (req, res) =>{
   var sql = 'INSERT INTO rota (id_usuario, id_TipoRota, id_origem, id_destino, qtdelugar, previsao, distancia,dataCriacao) VALUES (3,'+req.body.tipoRota+                                                                                    
@@ -168,7 +186,7 @@ router.post('/carona/novo', (req, res) =>{
 
 //PUT------------------------------------------------------
 router.put('/carona/update', (req, res) =>{
-  var sql = 'UPDATE carona  SET status = 1'+
+  var sql = 'UPDATE carona SET status = 1'+
                             ' WHERE id='+req.body.id;
   execSQLQuery(sql,res);
 })
