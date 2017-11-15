845ac5e290f7ee80859f79bc024d59f14a36e71a
machade <matheus-machado1@hotmail.com>
2017-11-15 20:07:31 -0200

    .

--- index.js
+++ index.js
@@ -54,45 +54,6 @@ function execSQLQuery(sqlQry, res){
   });
 }
 
-
-
-login = function(req,res){
-  var email= req.body.email;
-  var password = req.body.password;
-  connection.query('SELECT * FROM usuario WHERE email = ?',[email], function (error, results, fields) {
-  if (error) {
-    // console.log("error ocurred",error);
-    res.send({
-      "code":400,
-      "failed":"error ocurred"
-    })
-  }else{
-    // console.log('The solution is: ', results);
-    if(results.length >0){
-      if([0].senha == password){
-        res.send({
-          "code":200,
-          "success":"login sucessfull"
-            });
-      }
-      else{
-        res.send({
-          "code":204,
-          "success":"Email and password does not match"
-            });
-      }
-    }
-    else{
-      res.send({
-        "code":204,
-        "success":"Email does not exits"
-          });
-    }
-  }
-  });
-}
-
-router.post('/login', login)
 // GET ------------------------------------------------------
 router.get('/dest_universidade', (req, res) =>{
     execSQLQuery('SELECT * FROM local WHERE ID_USUARIO= 1', res);
